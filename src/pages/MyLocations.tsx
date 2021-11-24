2be603e60e257c6051207159043f41d121ee671a
Krystof Davidek <kry.davidek@gmail.com>
2021-11-24 16:43:51 +0100

    Implement API basics

--- src/pages/MyLocations.tsx
+++ src/pages/MyLocations.tsx
@@ -1,3 +1,23 @@
-const MyLocations = () => <div>My Locations</div>;
+import useSWR from 'swr';
+import { Typography, CircularProgress } from '@mui/material';
+import { useEffect } from 'react';
+
+import { fetcher } from '../utils/fetcher';
+
+const MyLocations = () => {
+	const { data, error } = useSWR(
+		`current.json?key=${process.env.REACT_APP_API_KEY}&q=Praha&aqi=no`,
+		fetcher
+	);
+
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
+	if (error) return <Typography>Failed to load data...</Typography>;
+	if (!data) return <CircularProgress />;
+
+	return <div>My Locations</div>;
+};
 
 export default MyLocations;
