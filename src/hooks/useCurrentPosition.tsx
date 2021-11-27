08542d9870d04a74583d9900d7fbb15fe38b2555
Ivana Papcunova <ivana.papcunova@wandera.com>
2021-11-28 00:58:32 +0100

    Current geolocation hook

---
+++ src/hooks/useCurrentPosition.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+const useCurrentPosition = () => {
+	const [position, setPosition] = useState({});
+	const [error, setError] = useState('');
+
+	const onChange = (geo: GeolocationPosition) => {
+		setPosition({
+			latitude: geo.coords.latitude,
+			longitude: geo.coords.longitude
+		});
+	};
+
+	const onError = (error: GeolocationPositionError) => {
+		setError(error.message);
+	};
+
+	// Handler enables the app to update the coords continually
+	useEffect(() => {
+		const geo = navigator.geolocation;
+		if (!geo) {
+			setError('Geolocation is not supported');
+			return;
+		}
+		const handler = geo.watchPosition(onChange, onError);
+		return () => geo.clearWatch(handler);
+	}, []);
+
+	return { ...position, error };
+};
+
+export default useCurrentPosition;
