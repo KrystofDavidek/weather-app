fb1bc4b846becfb9c15b4797299b02ddb658b868
Michal Čížek <cizek@inqool.cz>
2021-12-02 16:19:40 +0100

    added https

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://api.weatherapi.com/v1/';
+axios.defaults.baseURL = 'https://api.weatherapi.com/v1/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post.Accept = 'application/json';
 
